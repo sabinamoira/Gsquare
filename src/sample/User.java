cd1cb0f19ee927dc0ac522abf3fb0d4293d03973
Onwonga, Moraa <monwonga3@gatech.edu>
2018-12-28 22:57:16 -0500

    new

--- src/sample/User.java
+++ src/sample/User.java
@@ -7,6 +7,7 @@ package sample; /**
 /**
  * This class represents a User on the GSquare site.
  * A User can be a student (grad or undergrad), or an instructor
+ *
  * @author monwonga3 (Moraa Onwonga)
  * @version 1
  */
@@ -20,58 +21,63 @@ public abstract class User implements Comparable<User> {
     private static int idIndex = 1;
 
     /**
-    * This constructor creates a User for the site GSquare.
-    * It sets the User's name
-    * It sets the User's id number
-    * @param name the name of the User
-    */
+     * This constructor creates a User for the site GSquare.
+     * It sets the User's name
+     * It sets the User's id number
+     *
+     * @param name the name of the User
+     */
     public User(String name) {
         this.name = name;
         this.id = idIndex++;
     }
 
     /**
-    * Gets the class furthest down the tree that the User falls under
-    * This is used in GSquare to display the type of each class.
-    * @return a string representation of the Class name
-    */
+     * Gets the class furthest down the tree that the User falls under
+     * This is used in GSquare to display the type of each class.
+     *
+     * @return a string representation of the Class name
+     */
     public String getType() {
-        return this.getClass().getName();
+        String type = this.getClass().getName();
+        return type.replaceAll("sample.", "");
     }
 
     /**
-    * @return the name of the User
-    */
+     * @return the name of the User
+     */
     public String getName() {
         return name;
     }
 
     /**
-    * Gets the id representing the course name
-    * @return the course id number
-    */
+     * Gets the id representing the course name
+     *
+     * @return the course id number
+     */
     public int getId() {
         return id;
     }
 
     /**
-    * Compares two Users.
-    * Users with larger ids are greater, or equivalent if ids are equal
-    * If the User falls under a subclass than it compares their
-    * types, their class names, togehter
-    * @param other the 2nd User being compared
-    * @return an int that is positive if the 1st value is greter
-    * 0 if equal and negative if lesser
-    */
+     * Compares two Users.
+     * Users with larger ids are greater, or equivalent if ids are equal
+     * If the User falls under a subclass than it compares their
+     * types, their class names, together
+     *
+     * @param other the 2nd User being compared
+     * @return an int that is positive if the 1st value is greter
+     * 0 if equal and negative if lesser
+     */
     public int compareTo(User other) {
         if (other instanceof Student) {
             Student casted = (Student) other;
             return this.getClass().getName()
-                   .compareTo(casted.getClass().getName());
+                    .compareTo(casted.getClass().getName());
         } else if (other instanceof Instructor) {
             Instructor casted = (Instructor) other;
             return this.getClass().getName()
-                   .compareTo(casted.getClass().getName());
+                    .compareTo(casted.getClass().getName());
         } else {
             return ((Integer) this.getId()).compareTo((Integer) other.getId());
         }
